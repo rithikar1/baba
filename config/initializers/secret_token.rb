8b5512c7dfa70099be56f61dfc465a72a442afb2
Chanakya <chanakyadevraj@gmail.com>
2014-09-03 13:44:21 +0530

    baba site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baba::Application.config.secret_token = 'a5e557f0ee73f210a80b0284bee2e7be0c26b5aa339e43f1d6df9a6ea59b5a66a84e08f7157499681c601efadf58dc1ad656a985de6ac2e37361d39d49283541'
