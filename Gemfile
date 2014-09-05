09d8e150792980be5d22bc347b28364b1bccba84
rithikar1 <kunal_waghray@yahoo.co.in>
2014-09-05 18:43:23 +0530

    few more changes

--- Gemfile
+++ Gemfile
@@ -20,7 +20,15 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
-gem 'jquery-rails'
+# gem 'jquery-rails'
+
+gem "jquery-rails", "~> 2.3.0"
+
+
+
+gem 'activeadmin'
+
+
 
 gem 'zurb-foundation'
 # To use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Baba::Application.routes.draw do
+
+   root :to => 'welcome#index'
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   get "welcome/index"
   get "welcome/others"
   get "welcome/photos"
@@ -54,7 +60,6 @@ Baba::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
